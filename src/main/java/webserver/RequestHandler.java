af5d3aa4f6efb9e148dafdeda36237e02a98831a
Han <chelllagom@gmail.com>
2021-08-10 20:42:32 +0900

    feat: url 파싱 responseHeader에서 path를 추출하여 뷰 호출

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,9 +29,18 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            //일반적으로 헤더는 라인 단위로 읽는다(InputStream->InputStreamReader->BufferedReader)
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	//한줄 읽기
+        	String line = br.readLine();
+        	//null 처리
+        	if(line == null) return;
+        	//path 추출
+        	String url = HttpRequestUtils.getUrl(line);
+        	
+        	DataOutputStream dos = new DataOutputStream(out);
+        	// ./webapp+/index.html 전부 byte로 읽음
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
